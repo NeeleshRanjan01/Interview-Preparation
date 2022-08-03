b127df866a5a9e6815a5fff53fa088569afd3f28
Neelesh Ranjan <neeleshranjan95@gmail.com>
2022-08-04 03:10:16 +0530

    main

---
+++ JS_Interview_Prep_1/index.js
@@ -0,0 +1,35 @@
+//Q1. what is Javascript?
+
+// JavaScript is the Programming Language for the Web.JavaScript can update and change both HTML and CSS.JavaScript can calculate, manipulate and validate data. JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.</p><p>JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard. It has dynamic typing, prototype-based object-orientation, and first-class functions. It is multi-paradigm, supporting event-driven, functional, and imperative programming styles. It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM).
+
+
+//Q2. what is the javascript run time environment?
+
+//A runtime environment is where your program will be executed. 
+//The JavaScript engine works inside an environment, which provides additional features to your scripts that you can use at runtime.
+//JRE is responsible for making JavaScript asynchronous.
+
+
+//Q3. what is execution context?
+
+// Execution context is the concept for describing the internal working of a code.
+// In JavaScript, the environment that enables the JavaScript code to get executed is what we call JavaScript Execution Context. 
+// It is the execution context that decides which code section has access to the functions, variables, and objects used in the code.
+
+// Global Execution Context is also called the base/default execution. Any JavaScript code which does not reside in any function will be present in the global execution context.
+// Functional Execution Code is that type of context which is created by the JavaScript engine when any function call is found.
+
+// The Execution context mainly contain memory allocation and code execution phase. 
+
+
+//Q4. what is javascript Engine?
+
+// JavaScript is a multi-paradigm prototype-based language, which uses JavaScript Engine such as Chrome’s V8 engine Firefox SpiderMonkey engine and etc. They convert the high level code into machine-readable code which lets computer to perform some specific tasks. We will understand this using an image.
+
+// Ex. i) Google - V8   ii) Mozilla firefox - Spider Monkey    iii) Microsoft  -  Chakra
+
+
+//Q5. Flow of data in engine?
+
+// EcmaScript specification tells how JavaScript should be implemented by the browser so that a JavaScript program runs exactly the same in all the browsers, but it does not tell how JavaScript should run inside these browsers. It is up to the browser vendor to decide.
+// Every browser provides a JavaScript engine that runs the JavaScript code. The Netscape browser used the SpiderMonkey JavaScript engine. This engine was a rudimentary interpreter with no optimizations. Running the JavaScript code with this engine was slow but it worked.
